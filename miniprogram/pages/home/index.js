fcb6e2e854080387a5d1a3fedbf145dbbb290d33
zhudazhuang <1270049837@qq.com>
2023-02-01 12:39:23 +0800

    更新了轮播图demo和公告demo

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -1,73 +1,99 @@
-// pages/home/index.js
-Page({
+// // pages/home/index.js
+// Page({
 
     
-    /**
-     * 页面的初始数据
-     */
-    data: {
-      userInfo:{}
-    },
-    getUserInfo(res){
-      this.setData({
-        userInfo:res.detail.userInfo
-      })
-      console.log(res);
-    },
+//     /**
+//      * 页面的初始数据
+//      */
+//     data: {
+//       userInfo:{}
+//     },
+    // getUserInfo(res){
+    //   this.setData({
+    //     userInfo:res.detail.userInfo
+    //   })
+    //   console.log(res);
+    // },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+//     /**
+//      * 生命周期函数--监听页面加载
+//      */
+//     onLoad(options) {
 
-    },
+//     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+//     /**
+//      * 生命周期函数--监听页面初次渲染完成
+//      */
+//     onReady() {
 
-    },
+//     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+//     /**
+//      * 生命周期函数--监听页面显示
+//      */
+//     onShow() {
 
-    },
+//     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+//     /**
+//      * 生命周期函数--监听页面隐藏
+//      */
+//     onHide() {
 
-    },
+//     },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+//     /**
+//      * 生命周期函数--监听页面卸载
+//      */
+//     onUnload() {
 
-    },
+//     },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
+//     /**
+//      * 页面相关事件处理函数--监听用户下拉动作
+//      */
+//     onPullDownRefresh() {
 
-    },
+//     },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
+//     /**
+//      * 页面上拉触底事件的处理函数
+//      */
+//     onReachBottom() {
 
-    },
+//     },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
+//     /**
+//      * 用户点击右上角分享
+//      */
+//     onShareAppMessage() {
 
+//     }
+// })
+
+Page({
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
     }
+  },
+
+  data: {
+    userInfo:{},
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500
+  },
+  getUserInfo(res){
+    this.setData({
+      userInfo:res.detail.userInfo
+    })
+    console.log(res);
+  },
+
 })
