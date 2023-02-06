d6e94a7c32716dccf0b2150555c18b9008022ba4
hxr <1481991555@qq.com>
2023-02-06 15:10:32 +0800

    修改公共样式、发起的活动修改样式

---
+++ cloudfunctions/user/index.js
@@ -0,0 +1,29 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const getMyActivityRecordList = require('./getMyActivityRecordList')
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    // event: 前端传过来的参数；  
+    const { type } = event;
+    const wxContext = cloud.getWXContext()
+    const res = {
+        status: 1,
+        msg:'success',
+    };
+    switch(type){
+        case 'getMyActivityRecordList':
+            res.data = await getMyActivityRecordList.main(event, context); 
+            // res.data = {
+            //     openid: wxContext.OPENID
+            // }
+            return res;
+        default:
+            res.status = 0;
+            res.msg =  '接口不存在';
+            res.data = null;
+            break;
+    }
+}
--- miniprogram/pages/launchActivities/index.js
+++ miniprogram/pages/launchActivities/index.js
@@ -5,51 +5,61 @@ Page({
      * 页面的初始数据
      */
     data: {
-    activitList:[
-        {
-           id:1,
-           lable:'消消乐哈哈',
-           state:'进行中...'
-        },
-        {
-           id:2,
-           lable:'消消乐哈哈',
-           state:'进行中...'
-        },
-        {
-           id:3,
-           lable:'消消乐哈哈',
-           state:'进行中...'
-        },
-        {
-           id:4,
-           lable:'消消乐哈哈',
-           state:'进行中...'
-        },
-        {
-           id:5,
-           lable:'消消乐哈哈',
-           state:'进行中...'
-        },
-    ],
-    activityLimitList: [{
-        id: 1,
-        imgUrl:'https://7a6c-zliu-dev-4gclbljp64cb5cd3-1302106483.tcb.qcloud.la/static/activity/img-activity.png?sign=92b504f8dbc1315a2db61d1da53d766f&t=1675306209',
-        
-      },
-      {
-        id: 2,
-        imgUrl:'https://7a6c-zliu-dev-4gclbljp64cb5cd3-1302106483.tcb.qcloud.la/static/activity/img-activity.png?sign=92b504f8dbc1315a2db61d1da53d766f&t=1675306209',
-      },
-      {
-        id: 3,
-        imgUrl:'https://7a6c-zliu-dev-4gclbljp64cb5cd3-1302106483.tcb.qcloud.la/static/activity/img-activity.png?sign=92b504f8dbc1315a2db61d1da53d766f&t=1675306209',
-      },
-     
-    ],
-    currentData : 0,
+        activitList: [{
+                id: 1,
+                lable: '消消乐哈哈',
+                state: '进行中...'
+            },
+            {
+                id: 2,
+                lable: '消消乐哈哈',
+                state: '进行中...'
+            },
+            {
+                id: 3,
+                lable: '消消乐哈哈',
+                state: '进行中...'
+            },
+            {
+                id: 4,
+                lable: '消消乐哈哈',
+                state: '进行中...'
+            },
+            {
+                id: 5,
+                lable: '消消乐哈哈',
+                state: '进行中...'
+            },
+        ],
+        activityLimitList: [
+            {
+                id: 1,
+                title: '帅娃娃哈哈哈',
+                imgUrl: 'https://7a6c-zliu-dev-4gclbljp64cb5cd3-1302106483.tcb.qcloud.la/static/activity/img-activity.png?sign=92b504f8dbc1315a2db61d1da53d766f&t=1675306209',
+            },
+            {
+                id: 2,
+                title: '乌拉拉魔法棒',
+                imgUrl: 'https://7a6c-zliu-dev-4gclbljp64cb5cd3-1302106483.tcb.qcloud.la/static/activity/img-activity.png?sign=92b504f8dbc1315a2db61d1da53d766f&t=1675306209',
+            },
+            {
+                id: 3,
+                title: '叫你一声你敢答应吗',
+                imgUrl: 'https://7a6c-zliu-dev-4gclbljp64cb5cd3-1302106483.tcb.qcloud.la/static/activity/img-activity.png?sign=92b504f8dbc1315a2db61d1da53d766f&t=1675306209',
+            },
+        ],
+        currentData: 0,
+        dateStartDay: '2023-01-01',
+        dateEndDay: '2023-01-01',
+        prizeSettingList: [{
+            id: 1,
+            prizeMapIcon: '../../images/icon-add_p.png',
+            prizeName: '奖品名称',
+            prizeNum: '奖品数量',
+            prizePeople: '助力人数'
+        }, ]
     },
-    
+
 
     /**
      * 生命周期函数--监听页面加载
@@ -57,36 +67,54 @@ Page({
     onLoad(options) {
 
     },
-    bindchange:function(e){
-        const that  = this;
-        that.setData({
-          currentData: e.detail.current
-        })
-      },
-      //点击切换，滑块index赋值
-      checkCurrent:function(e){
+    bindchange: function (e) {
         const that = this;
-     
-        if (that.data.currentData === e.target.dataset.current){
+        that.setData({
+            currentData: e.detail.current
+        })
+    },
+    //点击切换，滑块index赋值
+    checkCurrent: function (e) {
+        const that = this;
+
+        if (that.data.currentData === e.target.dataset.current) {
             return false;
-        }else{
-     
-          that.setData({
-            currentData: e.target.dataset.current
-          })
+        } else {
+
+            that.setData({
+                currentData: e.target.dataset.current
+            })
         }
-      },
-     to(){
-         wx.navigateTo({
-           url: '/pages/launchActivities/index',
-         })
-     },
-    
-     updataTo(){
+    },
+    //创建活动成功跳回活动页
+    to() {
+        wx.navigateTo({
+            url: '/pages/launchActivities/index',
+        })
+    },
+    //跳转编辑活动
+    updataTo() {
         wx.navigateTo({
             url: '/pages/updataActivities/index',
-          })
-     },
+        })
+    },
+    //开始时间
+    dateChangestart(e) {
+        console.log('值为', e.detail.value);
+        this.setData({
+            dateStartDay: e.detail.value
+
+        });
+    },
+    //结束时间
+    dateChangeEnd(e) {
+        console.log('jieshu', e.detail.value);
+        this.setData({
+            dateEndDay: e.detail.value
+
+        });
+    },
+
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
