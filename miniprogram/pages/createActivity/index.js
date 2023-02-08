216d1c0abdf26ad7b5d1cf0954e29a4ea966c8b9
hxr <1481991555@qq.com>
2023-02-08 08:23:59 +0800

    早起一推

--- miniprogram/pages/createActivity/index.js
+++ miniprogram/pages/createActivity/index.js
@@ -5,7 +5,17 @@ Page({
      * 页面的初始数据
      */
     data: {
-        nameValue:'',
+        form:{
+            nameValue: '',
+            titleValue: '',
+            dateStartDay: '2023-01-01',
+            dateEndDay: '2023-01-01',
+            radioValue: '',
+            textareaValue: "",
+            prizeName: '',
+            prizeNum: '',
+            peopleNum:'',
+        },
         prizeSettingList: [{
             id: 1,
             prizeMapIcon: '../../images/icon-add_p.png',
@@ -15,7 +25,7 @@ Page({
         }, ]
     },
     //点击上传活动图
-    upload(){
+    upload() {
         let _this = this;
         //唤起图片权限
         wx.chooseMedia({
@@ -52,12 +62,64 @@ Page({
             }
         })
     },
-    inputname(e){
-      this.setData({
-        nameValue:e.detail.value
-      })
-      console.log(this.data.nameValue);
+    inputName(e) {
+        this.setData({
+            nameValue: e.detail.value
+        })
+        console.log(this.data.nameValue);
     },
+    inputTitle(e) {
+        this.setData({
+            titleValue: e.detail.value
+        })
+        console.log(this.data.titleValue);
+    },
+    //开始时间
+    dateChangestart(e) {
+        console.log('值为', e.detail.value);
+        this.setData({
+            dateStartDay: e.detail.value
+
+        });
+    },
+    //结束时间
+    dateChangeEnd(e) {
+        console.log('jieshu', e.detail.value);
+        this.setData({
+            dateEndDay: e.detail.value
+        });
+    },
+    handleChange(e) {
+        this.setData({
+            radioValue: e.detail.value
+        });
+        console.log(e.detail.value);
+    },
+    inputTextarea(e) {
+        this.setData({
+            textareaValue: e.detail.value
+        });
+        console.log(e.detail.value);
+    },
+    getPrizeName(e) {
+        this.setData({
+            textareaValue: e.detail.value
+        });
+        console.log(e);
+    },
+    getPrizeNum(e) {
+        this.setData({
+            textareaValue: e.detail.value
+        });
+        console.log(e);
+    },
+    getPeopleNum(e) {
+        this.setData({
+            textareaValue: e.detail.value
+        });
+        console.log(e);
+    },
+
     /**
      * 生命周期函数--监听页面加载
      */
