c8f544c6765262eb58a186c9df4b1d2f39ee25bf
wpp <357281221@qq.com>
2023-02-09 09:08:37 +0800

    更新代码

--- cloudfunctions/merchantInfo/addMerchantInfo/index.js
+++ cloudfunctions/merchantInfo/addMerchantInfo/index.js
@@ -5,6 +5,7 @@ cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
+    const wxContext = cloud.getWXContext();
      await db.collection('merchantInfo').add({
         data: {
             merchantName: event.merchantName,
@@ -13,7 +14,8 @@ exports.main = async (event, context) => {
             storeName: event.storeName,
             storeClass: event.storeClass,
             storeAddress: event.storeAddress,
-            license: event.license
+            license: event.license,
+            openId:wxContext.OPENID,
         }
     })
     return {
