d10c3c98af469b655b9f212ffa4780dc81225ee2
wpp <357281221@qq.com>
2023-02-10 09:57:43 +0800

    拉取页面

--- cloudfunctions/activity/index.js
+++ cloudfunctions/activity/index.js
@@ -1,7 +1,8 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 const createActivity = require('./createActivity')
-
+const getActivityList = require('./getActivityList')
+const getActivityDetail = require('./getActivityDetail')
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 
 // 云函数入口函数
@@ -11,19 +12,31 @@ exports.main = async (event, context) => {
     const wxContext = cloud.getWXContext()
     const res = {
         status: 1,
-        msg:'success',
+        msg: 'success',
     };
-    switch(type){
+    switch (type) {
         case 'createActivity':
-            res.data = await createActivity.main(event, context); 
+            res.data = await createActivity.main(event, context);
             // res.data = {
             //     openid: wxContext.OPENID
             // }
             return res;
         default:
             res.status = 0;
-            res.msg =  '接口不存在';
+            res.msg = '接口不存在';
             res.data = null;
             break;
+        case 'getActivityList':
+            res.data = await getActivityList.main(event, context);
+            // res.data = {
+            //     openid: wxContext.OPENID
+            // }
+            return res;
+        case 'getActivityDetail':
+            res.data = await getActivityDetail.main(event, context);
+            // res.data = {
+            //     openid: wxContext.OPENID
+            // }
+            return res;
     }
 }
