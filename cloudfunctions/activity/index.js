4076b53f0fce55260027ae76ae5d5a813f3b2364
MGX <272773670@qq.com>
2023-02-10 22:54:41 +0800

    完成活动列表接口的调试和数据渲染

--- cloudfunctions/activity/index.js
+++ cloudfunctions/activity/index.js
@@ -1,29 +1,35 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-const createActivity = require('./createActivity')
+const createActivity = require('./createActivity');
+const getList = require('./getList');
 
-cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+cloud.init({
+    env: cloud.DYNAMIC_CURRENT_ENV
+}) // 使用当前云环境
 
 // 云函数入口函数
 exports.main = async (event, context) => {
     // event: 前端传过来的参数；  
-    const { type } = event;
+    const {
+        type
+    } = event;
     const wxContext = cloud.getWXContext()
     const res = {
         status: 1,
-        msg:'success',
+        msg: 'success',
     };
-    switch(type){
+    switch (type) {
         case 'createActivity':
-            res.data = await createActivity.main(event, context); 
-            // res.data = {
-            //     openid: wxContext.OPENID
-            // }
-            return res;
+            res.data = await createActivity.main(event, context);
+            break;
+        case 'getList':
+            res.data = await getList.main(event, context);
+            break;
         default:
             res.status = 0;
-            res.msg =  '接口不存在';
+            res.msg = '接口不存在';
             res.data = null;
             break;
     }
+         return res;
 }
