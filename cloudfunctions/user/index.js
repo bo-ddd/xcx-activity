2264f361d7acb9afa8fdcaa9203bbff323e3db07
hxr <1481991555@qq.com>
2023-02-11 14:34:27 +0800

    登录获取用户信息

--- cloudfunctions/user/index.js
+++ cloudfunctions/user/index.js
@@ -1,8 +1,8 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 const getMyActivityRecordList = require('./getMyActivityRecordList')
-
 const getUserInfo = require('./getUserInfo')
+const addUserInfo = require('./addUserInfo')
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 
 // 云函数入口函数
@@ -17,14 +17,13 @@ exports.main = async (event, context) => {
     switch(type){
         case 'getMyActivityRecordList':
             res.data = await getMyActivityRecordList.main(event, context); 
-            // res.data = {
-            //     openid: wxContext.OPENID
-            // }
             return res;
         case 'getUserInfo':
             res.data = await getUserInfo.main(event, context); 
             return res;
-      
+        case 'addUserInfo':
+            res.data = await addUserInfo.main(event, context); 
+            return res;
         default:
             res.status = 0;
             res.msg =  '接口不存在';
