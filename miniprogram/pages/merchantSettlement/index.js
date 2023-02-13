a863be476e7d00e42a1c7595be835abb7693d242
wpp <357281221@qq.com>
2023-02-13 09:26:03 +0800

    修改页面逻辑

--- miniprogram/pages/merchantSettlement/index.js
+++ miniprogram/pages/merchantSettlement/index.js
@@ -152,9 +152,9 @@ Page({
                 wx.cloud.uploadFile({
                     cloudPath: 'merchant/' + new Date().toLocaleString() + '.png',
                     filePath: tempFilePaths,
-                    config: {
-                        env: 'zliu-dev-4gclbljp64cb5cd3'
-                    }, //不可以这么写，这样写会造成线上环境出现重大问题
+                    // config: {
+                    //     env: 'zliu-dev-4gclbljp64cb5cd3'
+                    // }, //不可以这么写，这样写会造成线上环境出现重大问题
                     success(res) {
                         that.setData({
                             fileId: res.fileID 
