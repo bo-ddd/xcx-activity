cfcb955f890a394ef3e0656380198fb82b9e170d
hxr <1481991555@qq.com>
2023-02-14 11:00:14 +0800

    参与活动功能

--- cloudfunctions/activity/getParticipateList/index.js
+++ cloudfunctions/activity/getParticipateList/index.js
@@ -8,11 +8,21 @@ const db = cloud.database();
 // 云函数入口函数
 exports.main = async (event, context) => {
     const wxContext = cloud.getWXContext();
+    const openid = wxContext.OPENID;
     try {
-        const res = await db.collection('participatingActivitieList').where({
-            openId:wxContext.OPENID,
-        }).get();
-        return res.data
+        console.log('----------------我是连表查询---------')
+        console.log(openid);
+       const res=await db.collection('participatingActivitieList').aggregate()
+        .lookup({
+          from: 'activity',//被关联的表
+          localField: 'activityId',//当前表想要查的字段
+          foreignField: '_id',//关联表想要的字段
+          as: 'userParticipatingList',
+        })
+        .end()
+        console.log(res);
+        return res
+      
     } catch (err) {
         return err
     }
